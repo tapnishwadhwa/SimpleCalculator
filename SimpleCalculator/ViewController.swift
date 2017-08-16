60c3a0f08cc7bd4d763283a95f23d402ca22caf9
Tapnish Wadhwa <tapnish@Hotmail.com>
2017-08-16 15:54:02 -0400

    intialCommit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0;
+    
+    @IBOutlet weak var label: UILabel!
 
+    @IBAction func numbers(_ sender: UIButton) {
+        
+        label.text = label.text! + String(sender.tag-1)
+        numberOnScreen = Double(label.text!)!
+    }
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+    
+        if label.text != "" && sender.tag != 11 && sender.tag != 16{
+            
+            if sender.tag == 12{
+                //process divide
+            }
+            else if sender.tag == 13{
+                //process multiplication
+            }
+           else if sender.tag == 14{
+                //process minus
+            }
+           else if sender.tag == 15{
+                //process addition
+            }
+            
+            
+            
+            
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
