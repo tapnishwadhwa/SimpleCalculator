fad9d2cb3c220b18c7e3d51b046c724c07aa8504
Tapnish Wadhwa <tapnish@Hotmail.com>
2017-08-15 20:42:05 -0400

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Tapnish on 2017-08-15.
+//  Copyright © 2017 Tapnish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
